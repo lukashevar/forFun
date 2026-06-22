befae30f24f2cf7463dcdc6df933ca697c392f9d
lukashevar <184438185+lukashevar@users.noreply.github.com>
2026-06-22 14:32:21 +0300

    added functions for directions

--- snake/include/Direction.hpp
+++ snake/include/Direction.hpp
@@ -1,1 +1,32 @@
 #pragma once
+
+#include "Vector2i.hpp"
+
+enum class Direction {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
+inline Vector2i directionToVector(Direction dir) {
+	switch (dir) {
+		case Direction::UP: return Vector2i{ 0, -1 };
+		case Direction::DOWN: return Vector2i{ 0, 1 };
+		case Direction::LEFT: return Vector2i{ -1, 0 };
+		case Direction::RIGHT: return Vector2i{ 1, 0 };
+	}
+
+	return Vector2i{ 0, 0 };
+}
+
+inline bool isOpposite(Direction a, Direction b) {
+	switch (a) {
+	case Direction::UP: return b == Direction::DOWN;
+	case Direction::DOWN: return b == Direction::UP;
+	case Direction::LEFT: return b == Direction::RIGHT;
+	case Direction::RIGHT: return b == Direction::LEFT;
+	}
+
+	return false;
+}
