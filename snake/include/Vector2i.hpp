fa2b0acdad70c9dccdc1980130569deb98c29206
lukashevar <184438185+lukashevar@users.noreply.github.com>
2026-06-22 14:19:23 +0300

    added coordinates

--- snake/include/Vector2i.hpp
+++ snake/include/Vector2i.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+struct Vector2i {
+
+	int x = 0;
+	int y = 0;
+
+	bool operator==(const Vector2i& other) const {
+		return x == other.x && y == other.y;
+	}
+
+	bool operator!=(const Vector2i& other) const {
+		return !(*this == other);
+	}
+
+	Vector2i operator+(const Vector2i& other) const {
+		return Vector2i{ x + other.x, y + other.y };
+	}
+};
