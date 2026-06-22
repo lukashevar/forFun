f5bb035f1bdb01c8da7292db4579c7be3bc5ffed
lukashevar <184438185+lukashevar@users.noreply.github.com>
2026-06-22 14:37:18 +0300

    added header for snake logic

--- snake/include/Snake.hpp
+++ snake/include/Snake.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <deque>
+#include "Vector2i.hpp"
+#include "Direction.hpp"
+
+class Snake {
+public:
+	explicit Snake(Vector2i startPosition);
+
+	void setDirection(Direction newDirection);
+
+	void move(bool growing);
+
+	bool collidesWithSelf() const;
+
+	bool collidesWithBounds(int gridWidth, int gridHeight) const;
+
+	const Vector2i& getHead() const;
+	const std::deque<Vector2i>& getBody() const;
+	Direction getDirection() const;
+
+private:
+	std::deque<Vector2i> m_body;
+	Direction m_direction;
+	Direction m_pendingDirection;
+};
